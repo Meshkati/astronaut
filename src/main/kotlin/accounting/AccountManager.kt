316b7e80c07c0a51bcf2841b2d4d0b2ef6106376
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2019-03-12 20:10:10 +0330

    Init AccountManager class

---
+++ src/main/kotlin/accounting/AccountManager.kt
@@ -0,0 +1,10 @@
+package accounting
+
+/**
+ * This class is to manage account properties
+ * like registering, logging in and all other the
+ * stuffs that are appropriate to user accounts
+ */
+object AccountManager {
+    
+}
