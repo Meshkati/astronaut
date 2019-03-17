9267429ab93295dc3e99f4526c58452391713b74
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2019-03-17 14:57:59 +0330

    Implement connect method in SQLiteDriver

--- src/main/kotlin/database/SQLiteDriver.kt
+++ src/main/kotlin/database/SQLiteDriver.kt
@@ -2,9 +2,30 @@ package database
 
 import java.sql.Connection
 import java.sql.DriverManager
+import java.sql.SQLException
 
+/**
+ * This class provides all the stuffs about handling sqlite
+ */
 object SQLiteDriver {
-    fun connect() {
-        val connection = DriverManager.getConnection(Config.SQLITE_PATH)
+    private var connection: Connection? = null
+
+    /**
+     * Makes the sqlite connection
+     */
+    private fun connect(): Connection? {
+        if (connection != null) {
+            return connection
+        }
+        // Make a new connection to the db
+        try {
+            connection = DriverManager.getConnection(Config.SQLITE_PATH)
+            // Check tables and creates needed ones
+            createTables()
+        } catch (e: SQLException) {
+            println(e.message)
+        }
+
+        return connection
     }
 }
