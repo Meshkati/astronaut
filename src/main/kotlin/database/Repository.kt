5e3871b2950ab9fda71192f11e7ba76b8275797c
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2019-03-17 15:59:43 +0330

    Implement getAccountID and getSecretSeed in Repository

--- src/main/kotlin/database/Repository.kt
+++ src/main/kotlin/database/Repository.kt
@@ -17,4 +17,18 @@ object Repository {
         SQLiteDriver.insert("keyValue", "account_id", keyPair.accountId)
         SQLiteDriver.insert("keyValue", "secret_seed", String(keyPair.secretSeed))
     }
+
+    /**
+     * Get's the secret seed from db
+     */
+    fun getSecretSeed(): String {
+        return SQLiteDriver.getValue("keyValue", "secret_seed")
+    }
+
+    /**
+     * Gets the account ID from db
+     */
+    fun getAccountID(): String {
+        return SQLiteDriver.getValue("keyValue", "accout_id")
+    }
 }
