c50f93a739e1c13cd1daf9c154529c5d752a4e5a
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2019-03-18 11:34:44 +0330

    Add env mode to Config class

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -3,5 +3,13 @@ class Config {
         const val BOT_URL = "https://friendbot.stellar.org/"
         const val SERVER_URL = "https://horizon-testnet.stellar.org"
         const val SQLITE_PATH = "jdbc:sqlite:astronaut.db"
+        const val MODE = "TEST" // `PROD` for production
+    }
+
+    /**
+     * Checks if we're on test mode, to set the server, network and other properties
+     */
+    fun isOnTestMode(): Boolean {
+        return MODE == "TEST"
     }
 }
