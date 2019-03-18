1d38e254be376eba9a355266997e18255936c675
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2019-03-18 11:36:19 +0330

    Init PaymentHandler class

---
+++ src/main/kotlin/transaction/PaymentHandler.kt
@@ -0,0 +1,11 @@
+
+package transaction
+
+/**
+ * This class provides all the stuffs about handling payments,
+ * like sending payment to others, get mentioned about getting a payment
+ * and other things.
+ */
+object PaymentHandler {
+    
+}
