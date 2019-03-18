b6a7211a53d08c2a625641e95c578280955b0b75
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2019-03-18 12:22:55 +0330

    Implement checkAccountExists method in PaymentHandler

--- src/main/kotlin/transaction/PaymentHandler.kt
+++ src/main/kotlin/transaction/PaymentHandler.kt
@@ -7,7 +7,30 @@ package transaction
  * and other things.
  */
 object PaymentHandler {
-    
+    /**
+     * Checks if the account with provided account_id exists in the network
+     * We have to do it to prevent extra fees on transactions
+     * This operation doesn't take any fee
+     * So before every transaction, check the existency of the account
+     * @param accountID
+     */
+    fun checkAccountExists(accountID: String): Boolean {
+        // Preparing the server
+        prepareNetwork()
+        val server = Server(Config.SERVER_URL)
+        // Creating the keyPair of the user
+        val userToCheck = KeyPair.fromAccountId(accountID)
+        try {
+            server.accounts().account(userToCheck)
+        } catch (e: Exception) {
+            println(e.message)
+
+            return false
+        }
+
+        return true
+    }
+
     /**
      * Prepares the network for the server, either on the test network or other networks
      */
